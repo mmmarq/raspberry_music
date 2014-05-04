dbd750d01f5c88afa01f26d37f1f97183efb09d9
Marcelo Martim Marques <marcelo.martim@gmail.com>
2014-05-04 11:08:11 -0300

    add new script

--- set_pins.sh
+++ set_pins.sh
@@ -2,12 +2,14 @@ export RED=17
 export GREEN=18
 export BLUE=27
 export SOUND=24
-export EXTRA=25
 
-for i in $RED $GREEN $BLUE $SOUND $EXTRA
+for i in $RED $GREEN $BLUE $SOUND 
 do
   gpio -g mode $i out
   gpio -g write $i 0
   gpio -g write $i 1
+  sleep 2
+  gpio -g write $i 0
+  sleep 2
 done
 
